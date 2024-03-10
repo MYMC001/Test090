b424c0e0e2016a4f7350f981ee55e92a9841f2b0
MYMC001 <152509083+MYMC001@users.noreply.github.com>
2024-03-10 22:55:06 +0300

    Update a.java

--- a.java
+++ a.java
@@ -1,3 +1,17 @@
-jqsdnjbcjb
-    ekfcnwdnvjcndw
-    whbchwbdhvbhwdfvb
+import java.util.Scanner;
+
+public class Greeting {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter their name
+        System.out.print("Please enter your name: ");
+        String name = scanner.nextLine();
+
+        // Greet the user
+        System.out.println("Hello, " + name + "! Welcome to our program.");
+        
+        // Close the scanner
+        scanner.close();
+    }
+}
